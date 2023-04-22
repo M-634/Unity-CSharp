85d828943c08b318cc6a9c5d8b2f4eb01585a40c
M-634 <miyanagateru634@gmail.com>
2023-04-22 22:18:02 +0900

    ボタンコンポーネントの拡張クラスの作成

---
+++ Practice-CSharp/Assets/_Samples/UGUI/ButtonExtension.cs
@@ -0,0 +1,12 @@
+using UnityEngine.UI;
+
+namespace _Samples.UGUI
+{
+    /// <summary>
+    /// UnityEngine「Buttonコンポーネント」の拡張クラス.
+    /// </summary>
+    public class ButtonExtension : Button
+    {
+        //TODO@未実装
+    }
+}
